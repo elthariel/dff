09ba1b133f8d3598633f8b379fc69a1e78992484
cma <cma@digital-forensic.org>
2009-09-17 18:51:49 +0200

    dff file is generated, so removing it. moving launcher to ressources

--- ressources/linux_launcher.sh
+++ ressources/linux_launcher.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 #
-# DFF -- An open source Digital Forensics Framework.
-#
+# DFF -- An Open Source Digital Forensics Framework
 # Copyright (C) 2009 ArxSys
-#
-# Malinge Christophe <christophe.malinge@arxsys.fr>
-#
-# See http://www.digital-forensic.org for more information about
-# this project. Please do not directly contact
-# any of the maintainers of this project for assistance;
-# the project provides a web site, mailing lists and IRC
-# channels for your use.
-#
+# 
 # This program is free software, distributed under the terms of
-# the GNU General Public License Version 3. See the LICENSE file
+# the GNU General Public License Version 2. See the LICENSE file
 # at the top of the source tree.
+# 
+# See http://www.digital-forensic.org for more information about this
+# project. Please do not directly contact any of the maintainers of
+# DFF for assistance; the project provides a web site, mailing lists
+# and IRC channels for your use.
+# 
+# Author(s):
+#  Christophe Malinge <cma@digital-forensic.org>
 #
 #
 # This script starts DFF. Its right place is in /usr/bin or
