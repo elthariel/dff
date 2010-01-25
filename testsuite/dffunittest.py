baa74118dfa52b014b4fa0deb6f6a819b48d0f2c
cma <cma@digital-forensic.org>
2010-01-25 18:44:46 +0100

    This commit refs #17 : test automation. Adding 5 unit test validating local driver. Use ctest -V to execute.

---
+++ testsuite/dffunittest.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python 
+# DFF -- An Open Source Digital Forensics Framework
+# Copyright (C) 2009 ArxSys
+# 
+# This program is free software, distributed under the terms of
+# the GNU General Public License Version 2. See the LICENSE file
+# at the top of the source tree.
+# 
+# See http://www.digital-forensic.org for more information about this
+# project. Please do not directly contact any of the maintainers of
+# DFF for assistance; the project provides a web site, mailing lists
+# and IRC channels for your use.
+# 
+# Author(s):
+#  Christophe M. <cma@digital-forensic.org>
+#
+
+import unittest, sys, os, fcntl
+from cStringIO import StringIO
+
+sys.path.insert(0, sys.path[0] + '/../')
+
+if os.name == "posix": 
+   try :	
+      import dl
+      sys.setdlopenflags(sys.getdlopenflags() | dl.RTLD_GLOBAL)
+   except ImportError:
+      import ctypes
+      sys.setdlopenflags(sys.getdlopenflags() | ctypes.RTLD_GLOBAL)
+elif os.name == "nt":
+    graphical = 0 
+
+from ui.console.console import *
+from api.loader import *
+from api.vfs import vfs
+
+
+class DffUnittest(unittest.TestCase):
+    def setUp(self):
+        """ Initialize framework
+        Load modules
+        Load console without loop
+        Redirect stdout and stderr to readable fileobjects
+        """
+        sys.stdout = StringIO()
+        sys.stderr = StringIO()
+
+        l = loader.loader()
+
+        self._hook_streams(sys.__stdout__.fileno(), sys.__stderr__.fileno())
+        l.do_load('../modules/')
+        self._restore_streams()
+        
+        self.ui = console()
+        
+        self.vfs = vfs.vfs()
+        
+        # Close and re '.. = StringIO()' to drop output from modules loader
+        sys.stdout.close()
+        sys.stderr.close()
+        sys.stdout = StringIO()
+        sys.stderr = StringIO()
+
+    def tearDown(self):
+        """ Restore stdout and stderr before end of each
+        tests
+        """
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+    def _set_nonblock(self, fileobj):
+        """ Set a fileobject non-blocking
+        """
+        fd = fileobj.fileno()
+        n = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, n|os.O_NONBLOCK)
+
+    def _hook_streams(self, stdout, stderr):
+        """  Avoid output of driver in current shell
+        """
+        self.old_stdout = os.dup(stdout)
+        self.old_stderr = os.dup(stderr)
+
+        self.pipeOut = os.pipe()
+        self.pipeErr = os.pipe()
+
+        os.close(stdout)
+        os.dup2(self.pipeOut[1], stdout)
+        os.close(stderr)
+        os.dup2(self.pipeErr[1], stderr)
+
+        self.driverOut = os.fdopen(self.pipeOut[0])
+        self.driverErr = os.fdopen(self.pipeErr[0])
+        self._set_nonblock(self.driverOut)
+        self._set_nonblock(self.driverErr)
+
+    def _close(self, *fds):
+        for fd in fds:
+            if type(fd) == file:
+                fd.close()
+            else:
+                try:
+                    os.close(fd)
+                except:
+                    pass
+        
+    def _restore_streams(self):
+        """ Restore stdout and stderr for tests be able to display informations
+        Fetch stdout and stderr from driver and return both in a tuple
+        """
+
+        try:
+            readOut = self.driverOut.read(4096)
+        except:
+            readOut = ''
+        try:
+            readErr = self.driverErr.read(4096)
+        except:
+            readErr = ''
+
+        self.driverOut.flush()
+        self.driverErr.flush()
+        
+        os.dup2(self.old_stdout, sys.__stdout__.fileno())
+        os.dup2(self.old_stderr, sys.__stderr__.fileno())
+        self._close(self.old_stdout, self.old_stderr,
+                    self.driverOut, self.driverErr,
+                    self.pipeOut[1], self.pipeErr[1],
+                    self.pipeOut[0], self.pipeErr[0])
+
+        return (readOut, readErr)
+
---
+++ testsuite/local.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python 
+# DFF -- An Open Source Digital Forensics Framework
+# Copyright (C) 2009 ArxSys
+# 
+# This program is free software, distributed under the terms of
+# the GNU General Public License Version 2. See the LICENSE file
+# at the top of the source tree.
+# 
+# See http://www.digital-forensic.org for more information about this
+# project. Please do not directly contact any of the maintainers of
+# DFF for assistance; the project provides a web site, mailing lists
+# and IRC channels for your use.
+# 
+# Author(s):
+#  Christophe M. <cma@digital-forensic.org>
+#
+
+import unittest, sys, os, random
+from cStringIO import StringIO
+
+from dffunittest import DffUnittest
+
+class LocalTests(DffUnittest):
+    """ Tests on 'local' VFS driver
+    We load several things, and validate basic functionality of the framework.
+    Keep in mind that VFS loads are persistant from one test to another.
+
+    TODO
+    - seek
+    - negative seek
+    - open & close many files check fd reallocation
+    - load same file two times, open both, seek one, check both position are OK
+
+    """
+    
+    def __init__(self, name='runTest'):
+        unittest.TestCase.__init__(self, name)
+
+    testFile = '/etc/passwd'
+
+    def test01_LoadOneFile(self):
+        """ #01 Load a file present on every Unix
+        """
+        
+        # avoid output from driver loading in current stdout/stderr
+        self._hook_streams(sys.__stdout__.fileno(), sys.__stderr__.fileno())
+        
+        # launch command
+        self.ui.onecmd('local --path ' + self.testFile + ' --parent /')
+        
+        # get output
+        driverStdout, driverStderr = self._restore_streams()
+
+        # validate output from driver loading
+        self.assertEqual(driverStdout, '')
+        self.assertEqual(driverStderr, '')
+        
+        # validate output from framework
+        self.assertEqual(sys.stdout.getvalue(), self.testFile + '\nresult:\nno problem\n')
+        self.assertEqual(sys.stderr.getvalue(), '')
+
+    def test02_LsOneFile(self):
+        """ #02 ls previously loaded file and validate size
+        Check if --long (switch for size output) is right size of the file
+        """
+        
+        self.ui.onecmd('ls --long /')
+        
+        fileSize = str(os.stat(self.testFile).st_size)
+        expectedStdout = '/' + os.path.basename(self.testFile) + '\t' + fileSize + '\n'
+
+        self.assertEqual(sys.stdout.getvalue(), expectedStdout)
+        self.assertEqual(sys.stderr.getvalue(), '')
+        
+
+    def _itemsCount(self, path, onlyFiles = False):
+        """ Return number of file in a given directory
+        In case 'onlyFiles' is set, return only files count.
+        """
+        itemsCount = 0
+        for root, dirs, files in os.walk(path):
+            if onlyFiles:
+                itemsCount += len(files)
+            else:
+                itemsCount += len(dirs) + len(files)
+        return itemsCount
+
+    def test03_LoadOneDirectory(self):
+        """ #03 Load current working directory and validate number of files loaded
+        Make sure number of nodes loaded is the same number as number of file
+        stored in current working directory.
+        """
+
+        testDirectory = os.getcwd()
+        self._hook_streams(sys.__stdout__.fileno(), sys.__stderr__.fileno())
+        self.ui.onecmd('local --path ' + testDirectory + ' --parent /')
+        driverStdout, driverStderr = self._restore_streams()
+
+        expectedItems = str(self._itemsCount(os.getcwd()))
+        # Expected output after local execution
+        expectedStdout = testDirectory + '\nnodes created:\n' + expectedItems + '\nresult:\nno problem\n'
+
+        # Get number of nodes created, for message if first assert below failed
+        countPosStart = sys.stdout.getvalue().rfind('created:\n') + len('created:\n')
+        countPosEnd = sys.stdout.getvalue().find('\n', countPosStart)
+        loadedItems = sys.stdout.getvalue()[countPosStart:countPosEnd]
+
+        # validate output from driver loading
+        self.assertEqual(driverStdout, '')
+        self.assertEqual(driverStderr, '')
+        
+        # validate output from framework
+        self.assertEqual(sys.stdout.getvalue(), expectedStdout, "maybe hidden files exist in " + os.getcwd() + " (directory loaded by this test, expected: " + expectedItems + " items, loaded: " + loadedItems + ")")
+        self.assertEqual(sys.stderr.getvalue(), '')
+        
+    def test04_ReadFile(self):
+        """ #04 Read 128 byte in previously loaded file
+        Make sure content is equal on disk and in vfs
+        """
+        readAmount = 128
+        
+        with open(self.testFile, 'r') as f:
+            expectedContent = f.read(128)
+
+        checknode = self.vfs.getnode('/passwd')
+
+        nodeFile = checknode.open()
+        buff = nodeFile.read(128)
+        self.assertEqual(expectedContent, buff)
+
+    def _randomItem(self, path):
+        """ Return a random filepath in a given directory
+        """
+        itemsCount = self._itemsCount(path, True)
+        randomItemPosition = 0
+        while randomItemPosition == 0 or randomItemPosition > itemsCount:
+            randomItemPosition = int(random.random() * itemsCount)
+        i = 0
+        for root, dirs, files in os.walk(path):
+            j = 0
+            for j in range(len(files)):
+                i += 1
+                if i == randomItemPosition:
+                    return root + '/' + files[j]
+
+
+    def test05_RandomReadFile(self):
+        """ #05 Randomly check 100 file content
+        Read 'randomReadSize' from beginning of file and make sure
+        same content is obtained from disk and from VFS.
+        """
+
+        checkAmount = 100
+        while checkAmount:
+            # Obtain a random filepath from given directory
+            randomFilepath = self._randomItem(os.getcwd())
+            # Compute a random size to read
+            randomReadSize = int(random.random() * os.stat(randomFilepath).st_size)
+            # Open reference file
+            with open(randomFilepath, 'r') as f:
+                # Save reference data
+                expectedContent = f.read(randomReadSize)
+                # Set node path according to VFS format
+                vfsNodePath = '/' + randomFilepath[len(os.getcwd()) - len(os.path.basename(os.getcwd())):]
+                # Open node file
+                nodeFile = self.vfs.getnode(vfsNodePath).open()
+                # Save rode date from VFS
+                buff = nodeFile.read(randomReadSize)
+                # Close node file
+                nodeFile.close()
+                # Compare both data
+                self.assertEqual(expectedContent, buff)
+                # Reference file is automatically close ; 'with' statement
+            checkAmount -= 1
+
+    def runTest(self):
+        pass
+
+
+suite = unittest.TestSuite()
+suite.addTest(LocalTests('test01_LoadOneFile'))
+suite.addTest(LocalTests('test02_LsOneFile'))
+suite.addTest(LocalTests('test03_LoadOneDirectory'))
+suite.addTest(LocalTests('test04_ReadFile'))
+suite.addTest(LocalTests('test05_RandomReadFile'))
+unittest.TextTestRunner(verbosity=2).run(suite)
